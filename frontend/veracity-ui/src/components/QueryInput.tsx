dd7cc8ee0c22c9c8b81fcdd757249d8b66598859
Riya Vachhani <riya.vachhani0001@gmail.com>
2026-03-19 23:21:37 +0530

    modify

--- frontend/veracity-ui/src/components/QueryInput.tsx
+++ frontend/veracity-ui/src/components/QueryInput.tsx
@@ -142,7 +142,7 @@ export default function QueryInput({ onSubmit, isLoading }: QueryInputProps) {
                   <line x1="22" y1="2" x2="11" y2="13"/>
                   <polygon points="22 2 15 22 11 13 2 9 22 2"/>
                 </svg>
-                Analyse
+                send
               </>
             )}
           </button>
